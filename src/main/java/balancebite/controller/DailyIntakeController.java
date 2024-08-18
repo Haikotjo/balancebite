4c563dddefd3ec242b4a32371d7621e11473f4d8
Haikotjo <haikowierda@hotmail.com>
2024-08-18 21:01:04 +0200

    First version of daily intake and method to substract done.

---
+++ src/main/java/balancebite/controller/DailyIntakeController.java
@@ -0,0 +1,41 @@
+package balancebite.controller;
+
+import balancebite.model.DailyIntake;
+import balancebite.model.Meal;
+import balancebite.repository.DailyIntakeRepository;
+import balancebite.repository.MealRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/dailyintake")
+public class DailyIntakeController {
+
+    @Autowired
+    private DailyIntakeRepository dailyIntakeRepository;
+
+    @Autowired
+    private MealRepository mealRepository;
+
+    // POST endpoint om maaltijd van de daily intake af te trekken
+    @PostMapping("/subtractMeal/{mealId}")
+    public ResponseEntity<DailyIntake> subtractMealFromDailyIntake(@PathVariable Long mealId) {
+        // Haal de dagelijkse inname op, in dit geval de eerste die in de database staat
+        DailyIntake dailyIntake = dailyIntakeRepository.findById(1L)
+                .orElseThrow(() -> new RuntimeException("Daily intake not found"));
+
+        // Haal de maaltijd op die van de dagelijkse inname moet worden afgetrokken
+        Meal meal = mealRepository.findById(mealId)
+                .orElseThrow(() -> new RuntimeException("Meal not found"));
+
+        // Trek de maaltijd af van de dagelijkse inname
+        dailyIntake.subtractMealIntake(meal);
+
+        // Sla de geüpdatete dagelijkse inname op in de database
+        dailyIntakeRepository.save(dailyIntake);
+
+        // Retourneer de geüpdatete dagelijkse inname
+        return ResponseEntity.ok(dailyIntake);
+    }
+}
---
+++ src/main/java/balancebite/repository/DailyIntakeRepository.java
@@ -0,0 +1,10 @@
+package balancebite.repository;
+
+import balancebite.model.DailyIntake;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DailyIntakeRepository extends JpaRepository<DailyIntake, Long> {
+}
+
