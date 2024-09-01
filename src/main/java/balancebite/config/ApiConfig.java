60d0aee74d5baad4a8e1c0e5434052abcde0041c
Haikotjo <haikowierda@hotmail.com>
2024-09-01 15:59:17 +0200

    added javadocs

--- src/main/java/balancebite/config/ApiConfig.java
+++ src/main/java/balancebite/config/ApiConfig.java
@@ -4,14 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Configuration class for defining beans and retrieving API keys.
+ * This class provides a RestTemplate bean and a method to access the USDA API key.
+ */
 @Configuration
 public class ApiConfig {
 
+    /**
+     * Creates and configures a RestTemplate bean.
+     * RestTemplate is used to make HTTP requests in the application.
+     *
+     * @return A configured RestTemplate instance.
+     */
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
+    /**
+     * Retrieves the USDA API key from the system environment variables.
+     * The API key is used to authenticate requests to the USDA FoodData Central API.
+     *
+     * @return The USDA API key as a String.
+     */
     public String getUsdaApiKey() {
         return System.getenv("USDA_API_KEY");
     }
--- src/main/java/balancebite/config/FoodConfig.java
+++ src/main/java/balancebite/config/FoodConfig.java
@@ -5,15 +5,33 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+/**
+ * Configuration class to load FDC IDs from application properties.
+ * Binds properties with the prefix "food" to this class.
+ */
 @Configuration
 @ConfigurationProperties(prefix = "food")
 public class FoodConfig {
+
+    /**
+     * List of FoodData Central IDs loaded from the configuration.
+     */
     private List<String> fdcIds;
 
+    /**
+     * Gets the list of FDC IDs.
+     *
+     * @return The list of FDC IDs.
+     */
     public List<String> getFdcIds() {
         return fdcIds;
     }
 
+    /**
+     * Sets the list of FDC IDs.
+     *
+     * @param fdcIds The list of FDC IDs to set.
+     */
     public void setFdcIds(List<String> fdcIds) {
         this.fdcIds = fdcIds;
     }
