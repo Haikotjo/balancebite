829b2fc03d016937904251f08ed905b8e898d699
Haikotjo <haikowierda@hotmail.com>
2026-01-29 18:24:56 +0100

    Trying to fix my meals call v1.1

--- src/main/java/balancebite/specification/MealSpecifications.java
+++ src/main/java/balancebite/specification/MealSpecifications.java
@@ -5,6 +5,7 @@ import balancebite.model.meal.Meal;
 import balancebite.model.meal.references.Cuisine;
 import balancebite.model.meal.references.Diet;
 import balancebite.model.meal.references.MealType;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
 import java.util.List;
 
@@ -46,9 +47,13 @@ public class MealSpecifications {
     public static Specification<Meal> savedByUser(Long userId) {
         return (root, query, cb) -> {
             query.distinct(true);
-            return cb.equal(root.join("users").get("id"), userId);
+            return cb.equal(
+                    root.join("users", JoinType.LEFT).get("id"),
+                    userId
+            );
         };
     }
+
 
     public static Specification<Meal> hasCuisineIn(List<Cuisine> cuisines) {
         return (root, query, cb) -> {
